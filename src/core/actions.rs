0cad3384878d0a44c5ef9ce083702e4ab880ad58
AnshUjlayan <anshujlayan3@gmail.com>
2026-02-20 15:59:01 +0530

    add core actions to save word stats

---
+++ src/core/actions.rs
@@ -0,0 +1,33 @@
+use crate::core::{progress, session};
+use crate::ui::app::{App, Screen};
+use anyhow::{Result, anyhow};
+
+pub fn handle_enter(app: &mut App) -> Result<()> {
+    let session = app.session.as_mut().ok_or_else(|| anyhow!("No session"))?;
+
+    if !(session.show_definition && session.graded.is_some()) {
+        return Ok(());
+    }
+
+    let word = session.current_mut();
+    progress::update_word_stats(&app.conn, word)?;
+
+    let finished = session.advance();
+
+    if session.session_type == session::Type::Group {
+        progress::save_progress(
+            &app.conn,
+            (
+                app.current_screen,
+                session.current().group_id,
+                session.index,
+            ),
+        )?;
+    }
+
+    if finished {
+        app.current_screen = Screen::Test;
+    }
+
+    Ok(())
+}
--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,3 +1,4 @@
+pub mod actions;
 pub mod progress;
 pub mod session;
 pub mod utils;
